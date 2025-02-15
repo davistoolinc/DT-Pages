4d1cbb8c257386b0b091dd092932f136945731f7
R. Sam Craig <SAMCRAIG70@GMAIL.COM>
2025-02-14 16:10:52 -0800

    Init boilerplate
    Initial boilerplate design complete for small/mobile devices.
    TODO: add links, add other pages, scale up design for larger devices, move header/footer information to injected JS, contact information forum if time allows.

---
+++ index.js
@@ -0,0 +1,11 @@
+const footer = document.getElementById('footer')
+const copyrightArea = document.getElementById('copyright')
+const addressSection = document.getElementById('address')
+const dateObject = new Date()
+const currentYear = dateObject.getFullYear()
+
+
+function renderFooter() {
+    footer.innerHTML = `
+    `
+}
