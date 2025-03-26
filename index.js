48865fc7820aa9ea655ac094f5ccd0123beab5b4
R. Sam Craig <SAMCRAIG70@GMAIL.COM>
2025-03-25 22:00:50 -0700

    Footer styling update, aria-labels added
    Changed multiple aspects of the footer, with font icons representing all contact options on the left side and copyright moved to the right. Min-width removed from header img, now relies on height. Removed colon from index.html h1.

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function renderHeader() {
             ${renderNavLinks()}
         </ul>
 
-        <button type="button" class="hamburger right-aligned nav-btn">
+        <button type="button" class="hamburger right-aligned nav-btn" aria-label="Open navigation menu">
             <span class="nav-btn"></span>
             <span class="nav-btn"></span>
             <span class="nav-btn"></span>
@@ -52,26 +52,24 @@ function renderHeader() {
 
 function renderFooter() {
     footer.innerHTML = `
-        <p class="copyright" id="copyright"> 
+        <address id="contact-information">
+            <a href="tel:1-509-891-5568" aria-label="Call us"><span class="icon-phone"></span></a>
+            <a href="mailto:neal@davistoolinc.com?subject=Website Inquiry" aria-label="Email us"><span class="icon-envelop"></span></a>
+            <a href="https://www.google.com/maps/place/Davis+Tool+Inc/@47.6790295,-117.2716579,17z/data=!3m1!4b1!4m6!3m5!1s0x549e1f8a8be2aecb:0x881da9a98c04aead!8m2!3d47.6790259!4d-117.269083!16s%2Fg%2F11bvtb87ng?entry=ttu&g_ep=EgoyMDI1MDMyMy4wIKXMDSoASAFQAw%3D%3D"><span class="icon-location" aria-label="Directions"><span></a>
+        </address>
+
+        <p class="right-aligned" id="copyright"> 
             Davis Tool, Inc. &copy; ${currentYear} 
         </p>
-
-        <address class="right-aligned" id="address">
-            10111 E. Montgomery Ave.
-            <br />
-            Spokane Valley, WA 99206
-            <br />
-            &#128222 <a href="tel:1-509-891-5568"> (509) 891-5568</a>
-        </address>
     `
 }
 
 function renderNavLinks() {
     return `
-            <li><a class="menu-link" href="index.html">Home</a></li>
-            <li><a class="menu-link" href="markets.html">Markets</a></li>
-            <li><a class="menu-link" href="capabilities.html">Capabilities</a></li>
-            <li><a class="menu-link" href="facility.html">Facility</a></li>
-            <li><a class="menu-link" href="contact.html">Contact Us</a></li>
-        `
+        <li><a class="menu-link" href="index.html">Home</a></li>
+        <li><a class="menu-link" href="markets.html">Markets</a></li>
+        <li><a class="menu-link" href="capabilities.html">Capabilities</a></li>
+        <li><a class="menu-link" href="facility.html">Facility</a></li>
+        <li><a class="menu-link" href="contact.html">Contact Us</a></li>
+    `
 }
