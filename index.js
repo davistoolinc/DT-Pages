b72ae498ee0fd297797d9b51bab6b26a59ec672c
R. Sam Craig <SAMCRAIG70@GMAIL.COM>
2025-06-10 21:16:50 -0700

    Header updated, merged markets and capabilities.
    Header now displays the page title. After  thinking about the "above the fold" method, I decided to merge the Markets and Capabilities pages and removed the photo.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const copyrightArea = document.getElementById('copyright')
 const addressSection = document.getElementById('address')
 const dateObject = new Date()
 const currentYear = dateObject.getFullYear()
+const title = document.title
 
 renderContent()
 
@@ -30,8 +31,10 @@ function renderHeader() {
  header.innerHTML = `
     <section >
         <img src="Pictures/logo-img.jpg" alt="Davis Tool Incorporated logo">
+    </section>
 
-        <aside class="company-motto"> Quality, Service, Value </aside>
+    <section class="title">
+    <h1>${title}</h1>
     </section>
     <nav>
         <ul class="large-nav hidden">
@@ -55,12 +58,12 @@ function renderFooter() {
     footer.innerHTML = `
         <address id="contact-information">
             <a href="tel:1-509-891-5568" aria-label="Call us"><span class="icon-phone"></span></a>
-            <a href="mailto:neal@davistoolinc.com?subject=Website Inquiry" aria-label="Email us"><span class="icon-envelop"></span></a>
+            <a href="mailto:neal@davistoolinc.com?subject=Website Inquiry" target="_blank" aria-label="Email us"><span class="icon-envelop"></span></a>
             <a href="https://www.google.com/maps/place/Davis+Tool+Inc/@47.6790295,-117.2716579,17z/data=!3m1!4b1!4m6!3m5!1s0x549e1f8a8be2aecb:0x881da9a98c04aead!8m2!3d47.6790259!4d-117.269083!16s%2Fg%2F11bvtb87ng?entry=ttu&g_ep=EgoyMDI1MDMyMy4wIKXMDSoASAFQAw%3D%3D"><span class="icon-location" aria-label="Directions"><span></a>
         </address>
 
-        <p class="right-aligned" id="copyright"> 
-            Davis Tool, Inc. &copy; ${currentYear} 
+        <p class="right-aligned" id="copyright">
+            Davis Tool, Inc. &copy; ${currentYear}
         </p>
     `
 }
@@ -68,7 +71,7 @@ function renderFooter() {
 function renderNavLinks() {
     return `
         <li><a class="menu-link" href="index.html">Home</a></li>
-        <li><a class="menu-link" href="markets.html">Markets</a></li>
+        <!-- <li><a class="menu-link" href="markets.html">Markets</a></li> -->
         <li><a class="menu-link" href="capabilities.html">Capabilities</a></li>
         <li><a class="menu-link" href="facility.html">Facility</a></li>
         <li><a class="menu-link" href="contact.html">Contact Us</a></li>
